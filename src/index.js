185a07252f77f480e1e80a3e67b6d17115179923
Anastasiia <guba.anastaysha@gmail.com>
2022-11-10 20:01:28 +0200

    updated imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'App';
+import App from 'App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants/theme';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
